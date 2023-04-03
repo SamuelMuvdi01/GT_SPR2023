310f97fd3814093a28d681c4d64af10e4b501afb
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-04-02 21:40:44 -0400

    Added logic for array building and Manual entry

--- Controller.py
+++ Controller.py
@@ -16,3 +16,37 @@
 # CERTIFICATION: I certify that this work is my own and that
 # none of it is the work of any other person.
 #=============================================================================
+
+import numpy as np
+print("Enter (R)andom or (M)anual payoff enteries: ")
+entry = input()
+entry = entry.upper()
+print(entry)
+
+rows = int(input("Enter the number of rows: "))
+cols = int(input("Enter the number of cols: "))
+
+
+
+if entry == 'M':
+    print("Add payoffs for player 1 by rows: ")
+    p1_payoff = []
+    p2_payoff = []
+    pays = cols * rows
+    
+    for i in range(pays):
+        p1_payoff.append(int(input()))
+    p1_strat = np.array(p1_payoff).reshape(rows, cols)
+
+    print("Add payoffs for player 2 by rows: ")
+    for i in range(pays):
+        p2_payoff.append(int(input()))
+    p2_strat = np.array(p2_payoff).reshape(rows, cols)
+
+print(p1_strat, "\n")
+print(p2_strat, "\n")
+
+normal_form_game = np.hstack((p1_strat,p2_strat))
+print(normal_form_game)
+
+
