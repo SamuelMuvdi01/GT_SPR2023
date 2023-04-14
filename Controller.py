cca84d7408c8bfb64441ea4ee4d434014cafaa27
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-04-13 22:55:21 -0400

    Update Controller.py
    handling now H for columns for p1 and rows for p2
    
    Added logic for displaying player strategies and payoffs

--- Controller.py
+++ Controller.py
@@ -18,12 +18,19 @@
 #=============================================================================
 
 import numpy as np
+import pandas as pd
+
 print("Enter (R)andom or (M)anual payoff enteries: ")
 entry = input()
 entry = entry.upper()
 
 rows = int(input("Enter the number of rows: "))
 cols = int(input("Enter the number of cols: "))
+
+while rows < 1 or rows > 9 and cols < 1 or cols > 9:
+    print("\n!!! Please enter rows and columns of size 1 - 9 !!!\n")
+    rows = int(input("Enter the number of rows: "))
+    cols = int(input("Enter the number of cols: "))
 
 p1_strat = []
 p2_strat = []
@@ -43,6 +50,30 @@ if entry == 'M':
         p2_payoff.append(str(input()))
     p2_strat = np.array(p2_payoff).reshape(rows, cols)
 
+strategies_p1 = []
+for i in range(rows):
+    strategies_p1.append('A'+ str(i))
+
+strategies_p2 = []
+for i in range(cols):
+    strategies_p2.append('B'+ str(i))
+
+print("==="*pays)
+print("Player: Player 1's Strategies")
+print(strategies_p1)
+print("==="*pays)
+print("Player: Player 2's Strategies")
+print(strategies_p2)
+print("==="*pays)
+
+print("\n")
+print("Player 1's payoffs:\n", p1_payoff)
+print("===" * pays)
+print("player 2's payoffs:\n", p2_payoff)
+print("===" * pays)
+print("\n !!! DISPLAY NORMAL FORM !!!")
+
+
 for i in range(rows):
     for j in range (cols):
         if j == cols - 1:
@@ -50,38 +81,39 @@ for i in range(rows):
         else:
             print( "(" + str(p1_strat[i,j]) + "," + str(p2_strat[i,j]) + ")", end = " ")
 
+print("===" * pays)
+print("\n !!! Finding Nash EQ !!! \n")
+print("===" * pays)
 
-print("\nFinding Nash EQ\n")
+t_p1 = []
+max_p1 = []
+t_p1 = p1_strat.transpose(1, 0)
+max_p1 = list(map(max,t_p1[::,::]))
 
 
-max_p1 = p1_strat.max(axis = 0)
-#max_p1 = str(max(max_p1))
+max_p2 = []
+max_p2 = list(map(max,p2_strat[::,::]))
 
+print("Player 1's best strategies for Nash EQ:\n", max_p1)
+print("===" * pays)
+print("player 2's best strategies for Nash EQ:\n", max_p2)
+print("===" * pays)
 
-#max_p2 = list(map(max,p2_strat[::,::]))
-max_p2 = p1_strat.max(axis = 0)
-#max_p2 = str(max(max_p2))
-
-print(max_p1, max_p2)
 
 for i in range(len(p1_strat)):
     for k in range(len(max_p1)):
         p1_strat[p1_strat == max_p1[k]] = 'H'
 
 for i in range(len(p2_strat)):
-        for k in range(len(max_p2)):
-         p2_strat[p2_strat == max_p2[k]] = 'H'
+       for k in range(len(max_p2)):
+        p2_strat[p2_strat == max_p2[k]] = 'H'
 
+print("\n !!! Game Matrix with Nash EQ !!! \n")
+print("===" * pays)
 for i in range(rows):
     for j in range (cols):
         if j == cols - 1:
             print( "(" + str(p1_strat[i,j]) + "," + str(p2_strat[i,j]) + ")")
         else:
             print( "(" + str(p1_strat[i,j]) + "," + str(p2_strat[i,j]) + ")", end = " ")
-
-
-
-
-
-
-
+print("===" * pays)
